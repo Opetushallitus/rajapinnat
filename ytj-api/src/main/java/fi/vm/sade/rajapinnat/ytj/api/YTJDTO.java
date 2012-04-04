7fe4b1048e09cf7bf794b2e969c950e46618e79e
tkatva <tkatva@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-04-04 07:13:42 +0000

    Added new YTJOsoiteDTO and modified YTJDTO

--- ytj-api/src/main/java/fi/vm/sade/rajapinnat/ytj/api/YTJDTO.java
+++ ytj-api/src/main/java/fi/vm/sade/rajapinnat/ytj/api/YTJDTO.java
@@ -21,6 +21,15 @@ public class YTJDTO {
 
     private String nimi;
     private String ytunnus;
+    private String yritysmuoto;
+    private String yrityksenKieli;
+    private YTJOsoiteDTO postiOsoite;
+    private YTJOsoiteDTO kayntiOsoite;
+    private String sahkoposti;
+    private String www;
+    private String puhelin;
+    private String faksi;
+    
 
     public Integer getVersio() {
         return versio;
@@ -40,5 +49,117 @@ public class YTJDTO {
 
     public void setYtunnus(String ytunnus) {
         this.ytunnus = ytunnus;
+    }
+
+    /**
+     * @return the yritysmuoto
+     */
+    public String getYritysmuoto() {
+        return yritysmuoto;
+    }
+
+    /**
+     * @param yritysmuoto the yritysmuoto to set
+     */
+    public void setYritysmuoto(String yritysmuoto) {
+        this.yritysmuoto = yritysmuoto;
+    }
+
+    /**
+     * @return the yrityksenKieli
+     */
+    public String getYrityksenKieli() {
+        return yrityksenKieli;
+    }
+
+    /**
+     * @param yrityksenKieli the yrityksenKieli to set
+     */
+    public void setYrityksenKieli(String yrityksenKieli) {
+        this.yrityksenKieli = yrityksenKieli;
+    }
+
+    /**
+     * @return the postiOsoite
+     */
+    public YTJOsoiteDTO getPostiOsoite() {
+        return postiOsoite;
+    }
+
+    /**
+     * @param postiOsoite the postiOsoite to set
+     */
+    public void setPostiOsoite(YTJOsoiteDTO postiOsoite) {
+        this.postiOsoite = postiOsoite;
+    }
+
+    /**
+     * @return the kayntiOsoite
+     */
+    public YTJOsoiteDTO getKayntiOsoite() {
+        return kayntiOsoite;
+    }
+
+    /**
+     * @param kayntiOsoite the kayntiOsoite to set
+     */
+    public void setKayntiOsoite(YTJOsoiteDTO kayntiOsoite) {
+        this.kayntiOsoite = kayntiOsoite;
+    }
+
+    /**
+     * @return the sahkoposti
+     */
+    public String getSahkoposti() {
+        return sahkoposti;
+    }
+
+    /**
+     * @param sahkoposti the sahkoposti to set
+     */
+    public void setSahkoposti(String sahkoposti) {
+        this.sahkoposti = sahkoposti;
+    }
+
+    /**
+     * @return the www
+     */
+    public String getWww() {
+        return www;
+    }
+
+    /**
+     * @param www the www to set
+     */
+    public void setWww(String www) {
+        this.www = www;
+    }
+
+    /**
+     * @return the puhelin
+     */
+    public String getPuhelin() {
+        return puhelin;
+    }
+
+    /**
+     * @param puhelin the puhelin to set
+     */
+    public void setPuhelin(String puhelin) {
+        this.puhelin = puhelin;
+    }
+
+    /**
+     * @return the faksi
+     */
+    public String getFaksi() {
+        return faksi;
+    }
+
+    /**
+     * @param faksi the faksi to set
+     */
+    public void setFaksi(String faksi) {
+        this.faksi = faksi;
     }
 }
