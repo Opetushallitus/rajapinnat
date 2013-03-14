71528feb837eb3c2d979b6598c0ee6aeaebfe2e2
tkatva <tkatva@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2013-03-14 10:38:37 +0000

    OVT-3960

--- ytj-service/src/test/java/fi/vm/sade/rajapinnat/ytj/service/YTJServiceImplTest.java
+++ ytj-service/src/test/java/fi/vm/sade/rajapinnat/ytj/service/YTJServiceImplTest.java
@@ -10,6 +10,9 @@ import fi.vm.sade.rajapinnat.ytj.api.exception.YtjConnectionException;
 import fi.ytj.Kieli;
 import java.util.List;
 import org.junit.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import static org.junit.Assert.*;
 
 /**
@@ -17,6 +20,8 @@ import static org.junit.Assert.*;
  * @author Tuomas Katva
  */
 public class YTJServiceImplTest {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
     
     public YTJServiceImplTest() {
     }
@@ -59,6 +64,33 @@ public class YTJServiceImplTest {
         YTJServiceImpl instance = new YTJServiceImpl();
         List result = instance.findByYNimi(nimi, naytaPassiiviset, kieli);
         
+    }
+
+    /*Ignore this test so that YTJ is not called every time when project is compiled
+     * This test call YTJ-service with test credentials, note that
+     * credentials have maximum daily call limit so test is ignored by default
+     */
+    @Ignore
+    @Test
+    public void testFindByNimiSuccess() {
+
+        String nimi = "Katva";
+        boolean naytaPassiiviset = false;
+        YTJKieli kieli = YTJKieli.FI;
+        YTJServiceImpl instance = new YTJServiceImpl();
+        instance.setAsiakastunnus("oPHallitusOPH");
+        instance.setSalainenavain("39769FB6-465D-43B7-AB26-4D891428BB42");
+        try {
+            List<YTJDTO> results = instance.findByYNimi(nimi,naytaPassiiviset,kieli);
+            log.info("Got {} results", results.size());
+            /*for (YTJDTO dto:results) {
+                log.info("Organisaatio nimi: {}",dto.getNimi());
+            }*/
+            assertTrue(results.size() > 0);
+        } catch (Exception exp) {
+            log.info("Exception in findByNimiSuccess : {}",exp.toString());
+            assertTrue(false);
+        }
     }
 
     /**
