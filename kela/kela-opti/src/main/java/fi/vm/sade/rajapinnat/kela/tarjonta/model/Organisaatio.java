37585f418a9df37dcbe3dd7b225ac3d25a74086c
mholi <markus.holi@proactum.fi>
2013-05-23 10:25:46 +0200

    KJOH-456: kela opti tiedonsiirtoprojekti

---
+++ kela/kela-opti/src/main/java/fi/vm/sade/rajapinnat/kela/tarjonta/model/Organisaatio.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2012 The Finnish Board of Education - Opetushallitus
+ *
+ * This program is free software:  Licensed under the EUPL, Version 1.1 or - as
+ * soon as they will be approved by the European Commission - subsequent versions
+ * of the EUPL (the "Licence");
+ *
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://www.osor.eu/eupl/
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * European Union Public Licence for more details.
+ */
+package fi.vm.sade.rajapinnat.kela.tarjonta.model;
+
+import javax.persistence.*;
+
+/**
+ * 
+ * @author Markus
+ */
+@Entity
+@Table(name="organisaatio")
+public class Organisaatio {
+
+    @Id
+    @Column(name = "id", unique = true, nullable = false)
+    @GeneratedValue
+    private Long id;
+    
+    @Column(name = "opetuspisteenjarjnro")
+    private String opetuspisteenJarjNro;
+    
+    @Column(name = "oid")
+    private String oid;
+    
+    
+    public String getOid() {
+        return oid;
+    }
+
+    public void setOid(String oid) {
+        this.oid = oid;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOpetuspisteenJarjNro() {
+        return opetuspisteenJarjNro;
+    }
+
+    public void setOpetuspisteenJarjNro(String opetuspisteenJarjNro) {
+        this.opetuspisteenJarjNro = opetuspisteenJarjNro;
+    }
+    
+}
