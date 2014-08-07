f37c35581d731515696bd746793ee49c616e1db9
Janne Hupli <jhupliSpotify@gmail.com>
2014-08-07 12:02:04 +0300

    URJL-38

--- kela/kela-opti/src/main/java/fi/vm/sade/rajapinnat/kela/WriteOPTIOL.java
+++ kela/kela-opti/src/main/java/fi/vm/sade/rajapinnat/kela/WriteOPTIOL.java
@@ -52,14 +52,16 @@ public class WriteOPTIOL extends AbstractOPTIWriter {
 	public void composeRecords() throws IOException, UserStopRequestException {
 		for (OrganisaatioPerustieto ol : this.orgContainer.getOppilaitokset()) {
 			try {
-				this.writeRecord(ol, "02");
+				//this.writeRecord(ol, "02");
+				this.writeRecord(ol, OrgType.OPPILAITOS);
 			} catch (OPTFormatException e) {
 				LOG.error(String.format(errors[0], ol.getOid()+" "+ol.getNimi()));
 			}
 		}
 		for (OrganisaatioPerustieto tp : this.orgContainer.getToimipisteet()) {
 			try {
-				this.writeRecord(tp, "03");
+				//this.writeRecord(tp, "03");
+				this.writeRecord(tp, OrgType.TOIMIPISTE);
 			} catch (OPTFormatException e) {
 				LOG.error(String.format(errors[3], tp.getOid()+" "+tp.getNimi()));
 			}
@@ -69,15 +71,16 @@ public class WriteOPTIOL extends AbstractOPTIWriter {
 	@Override
 	public String composeRecord(Object... args) throws OPTFormatException {
 		OrganisaatioPerustieto curOppilaitos = (OrganisaatioPerustieto) args[0];
-		String organisaatioTyyppi = (String) args[1];
+		OrgType organisaatioTyyppi = (OrgType) args[1];
         Organisaatio orgE = kelaDAO.findOrganisaatioByOid(curOppilaitos.getOid());
         String record = String.format("%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s",//19 fields + EOL
                 getOppilaitosNro(curOppilaitos),//OPPIL_NRO
                 getOrgOid(orgE),
-                organisaatioTyyppi,
+                organisaatioTyyppi.equals(OrgType.OPPILAITOS) ? "02" : "03",
                 StringUtils.leftPad("", 10),//Koulutuksen jarjestajan tunnus
                 getYhteystietojenTunnus(orgE),//Yhteystietojen tunnus
-                getOppilaitostyyppitunnus(curOppilaitos),//OTY_ID
+                organisaatioTyyppi.equals(OrgType.OPPILAITOS) ? getOppilaitostyyppitunnus(curOppilaitos):
+                			  getOppilaitostyyppitunnus(this.orgContainer.getOppilaitosoidOppilaitosMap().get(curOppilaitos.getParentOid())),//OTY_ID
                 numFormatter("0", 10, null), //0-merkkeja
                 StringUtils.leftPad("", 5),//AMK_OPNRO
                 getKotikunta(orgE),//Oppilaitoksen kotikunta
