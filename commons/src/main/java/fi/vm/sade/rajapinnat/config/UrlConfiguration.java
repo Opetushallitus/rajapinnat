e5caff0facc19cb6c113f20051c8b1b1c4650abb
Tommi Helander <tommi.helander@gofore.com>
2016-11-30 09:43:45 +0200

    Add commons module with UrlConfiguratoin singleton and url property file

---
+++ commons/src/main/java/fi/vm/sade/rajapinnat/config/UrlConfiguration.java
@@ -0,0 +1,37 @@
+package fi.vm.sade.rajapinnat.config;
+
+import fi.vm.sade.properties.OphProperties;
+
+import java.nio.file.Paths;
+
+public class UrlConfiguration extends OphProperties {
+
+    private volatile static UrlConfiguration instance;
+
+    private UrlConfiguration() {
+        addFiles("/rajapinnat-oph.properties");
+        addOptionalFiles(Paths.get(System.getProperties().getProperty("user.home"), "/oph-configuration/common.properties").toString());
+    }
+
+    /**
+     * Get instance of UrlConfiguration.
+     *
+     * This method initializes a new instance of UrlConfiguration, if one has not yet been initialized.
+     * Initialization occurs lazily and uses double checked locking for thread safety
+     * (see Joshua Bloch: Effective Java, Item 71: Use lazy initialization judiciously).
+     *
+     * @return UrlConfiguration
+     */
+    public static UrlConfiguration getInstance() {
+        UrlConfiguration i = instance;
+        if (i == null) {
+            synchronized(UrlConfiguration.class) {
+                i = instance;
+                if (i == null) {
+                    instance = i = new UrlConfiguration();
+                }
+            }
+        }
+        return i;
+    }
+}
