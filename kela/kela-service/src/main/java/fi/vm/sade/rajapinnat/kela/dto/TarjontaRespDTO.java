b62fd6ce607c6efd733950e4a6eef9675cbb1e62
August Kilponen <august.kilponen@gmail.com>
2018-10-11 18:50:52 +0300

    kela-servicen poisto

--- kela/kela-service/src/main/java/fi/vm/sade/rajapinnat/kela/dto/TarjontaRespDTO.java
+++
@@ -1,8 +0,0 @@
-package fi.vm.sade.rajapinnat.kela.dto;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class TarjontaRespDTO {
-    public TarjontaKomoDTO result;
-}
--- kela/kela-service/src/test/resources/import-tarjonta.sql
+++
@@ -1,1 +0,0 @@
-insert into hakukohde (id, oid, kela_linja_koodi, kela_linja_tarkenne) values (999, '1.2.246.562.20.70402217699', 'Y15', '01');
